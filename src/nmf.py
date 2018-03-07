091942616ffe94f6484da9f5213d592ec93fdd44
Maulik Shah <shahmaulik09@gmail.com>
2018-03-07 13:52:38 -0500

    Resolving bugs for the matching with the output.Testing the results
    Test and Match.

--- src/nmf.py
+++ src/nmf.py
@@ -2,26 +2,25 @@ from pyspark import SparkContext,SparkConf
 from extraction import NMF
 import thunder as td
 import json
-from neurofinder import load, match
+from neurofinder import load, match,centers,shapes
 
 
 #Create the Spark Config and Context.
 conf = SparkConf().setAppName('P3ImageClassification')
 sc = SparkContext.getOrCreate(conf=conf)
-
 #Declare the output array.
 outputArray= []
 
 #Load data from the source path.
-#TODO: Need to remove hardcodings.
+#Need to make use command-line args.
 images = td.images.fromtif('/home/maulik/DataSciencePracticum/Project3/neurofinder.00.01/testimages', ext='tiff')
-images = td.series.fromarray(data,engine=sc)
+images = td.series.fromarray(images,engine=sc)
 
 #Run the NMF over the algorithms.
 nmf_algo = NMF(k=5, percentile=99, max_iter=50, overlap=0.1)
 nmf_model = nmf_algo.fit(images, chunk_size=(50,50), padding=(25,25))
 merged = nmf_model.merge(0.1)
-regions = [{'id': '00.01','coordinates': region.coordinates.tolist()} for region in merged.regions]
+regions = [{'id': 1,'coordinates': region.coordinates.tolist()} for region in merged.regions]
 #result = {'id': '00.01', 'regions': regions}
 outputArray.append(regions)
 
@@ -30,9 +29,11 @@ with open('/home/maulik/DataSciencePracticum/Project3/neurofinder.00.01/regions/
 
 print('Done')
 
-
+#Remove extra pairs of square-brackates from the output.json before running this.
 #Compare outputs.
-#TODO: Need to remove hardcodings.
-a = load('/home/maulik/DataSciencePracticum/Project3/neurofinder.00.01/regions/regions.json')
+a = load('/home/maulik/DataSciencePracticum/Project3/neurofinder.00.01/regions/regionstest.json')
 b = load('/home/maulik/DataSciencePracticum/Project3/neurofinder.00.01/regions/output.json')
-match(a, b)
+
+print(match(a, b))
+print(centers(a, b))
+print(shapes(a, b))
